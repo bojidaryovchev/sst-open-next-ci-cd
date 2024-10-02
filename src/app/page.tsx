f9dea2bb629a361f35278e8915b5066f72a84039
bojidaryovchev <blizzardcon10@gmail.com>
2024-10-02 22:33:36 +0300

    refactor: minor update to test envs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div className="w-32 h-32 bg-green-400 rounded-full"></div>
+        <div className="w-32 h-32 bg-orange-400 rounded-full"></div>
 
         <Image
           className="dark:invert"
