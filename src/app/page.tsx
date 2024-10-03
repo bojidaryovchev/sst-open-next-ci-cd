299571395892ca2aeda66ea2d7396180bdd0f460
bojidaryovchev <blizzardcon10@gmail.com>
2024-10-03 23:06:30 +0300

    feat: google sign in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import SignIn from "@/components/sign-in";
+import UserAvatar from "@/components/user-avatar";
 import Image from "next/image";
 
 export default function Home() {
@@ -5,6 +7,10 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div className="w-32 h-32 bg-orange-400 rounded-full"></div>
+
+        <SignIn />
+
+        <UserAvatar />
 
         <Image
           className="dark:invert"
---
+++ src/components/user-avatar.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import { auth } from "../auth";
+
+export default async function UserAvatar() {
+  const session = await auth();
+
+  if (!session?.user?.image) return null;
+
+  return (
+    <>
+      <Image
+        src={session.user.image}
+        alt="User Avatar"
+        width={42}
+        height={42}
+      />
+    </>
+  );
+}
