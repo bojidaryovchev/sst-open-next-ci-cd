9cdab020f07077fbbe1226afaa26cd9f67c87645
bojidaryovchev <blizzardcon10@gmail.com>
2024-10-04 19:59:50 +0300

    refactor: auth improvements

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,10 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
-import google from "next-auth/providers/google";
+import authConfig from "./auth.config";
 import prisma from "./prisma";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [google],
+  session: { strategy: "jwt" },
+  ...authConfig,
 });
