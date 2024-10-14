c847bee74edfc2a513967e910ec7efee8e6df44e
bojidaryovchev <blizzardcon10@gmail.com>
2024-10-14 05:17:21 +0300

    chore: sst config for AWS SES

--- sst.config.ts
+++ sst.config.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="./.sst/platform/config.d.ts" />
 
 const isProd = (stage: string) => stage.startsWith("prod");
@@ -11,9 +12,18 @@ export default $config({
     };
   },
   async run() {
+    const domainName = isProd($app.stage) ? "justpurrrfect.com" : `${$app.stage}.justpurrrfect.com`;
+
+    const email = new sst.aws.Email("MyEmail", {
+      sender: `noreply@${domainName}`,
+    });
+
+    const SES_IDENTITY_ARN = email.nodes.identity.arn.get();
+    const SES_CONFIGURATION_SET_NAME = email.nodes.identity.configurationSetName.get();
+
     new sst.aws.Nextjs("OpenNext", {
       domain: {
-        name: isProd($app.stage) ? "justpurrrfect.com" : `${$app.stage}.justpurrrfect.com`,
+        name: domainName,
       },
       environment: {
         AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID!,
@@ -21,7 +31,11 @@ export default $config({
         AUTH_SECRET: process.env.AUTH_SECRET!,
         AUTH_TRUST_HOST: process.env.AUTH_TRUST_HOST!,
         DATABASE_URL: process.env.DATABASE_URL!,
+        SES_IDENTITY_ARN: SES_IDENTITY_ARN!,
+        SES_CONFIGURATION_SET_NAME: SES_CONFIGURATION_SET_NAME!,
+        AWS_REGION: process.env.AWS_REGION!,
       },
+      link: [email],
     });
   },
 });
