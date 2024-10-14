e53d1fc9d1d3aa15a59dc11ddea174f1af50300a
bojidaryovchev <blizzardcon10@gmail.com>
2024-10-14 05:49:40 +0300

    chore: sst config for AWS SES

--- sst.config.ts
+++ sst.config.ts
@@ -14,6 +14,10 @@ export default $config({
   async run() {
     const domainName = isProd($app.stage) ? "justpurrrfect.com" : `${$app.stage}.justpurrrfect.com`;
 
+    const email = new sst.aws.Email("OpenNextEmail", {
+      sender: `noreply@${domainName}`,
+    });
+
     new sst.aws.Nextjs("OpenNext", {
       domain: {
         name: domainName,
@@ -25,6 +29,7 @@ export default $config({
         AUTH_TRUST_HOST: process.env.AUTH_TRUST_HOST!,
         DATABASE_URL: process.env.DATABASE_URL!,
       },
+      link: [email],
     });
   },
 });
