c8ae21b9ad44ea59c5be6e71aeee169defe5f876
bojidaryovchev <blizzardcon10@gmail.com>
2024-10-14 06:17:16 +0300

    refactor: trying things out

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ const HomePage: React.FC = async () => {
         <UserAvatar />
 
         <form
-          action={() => {
+          action={async () => {
             "use server";
             console.log(process.env);
           }}
