60529d088151c0407f949cb7b04b7ab0da2f3d56
bojidaryovchev <blizzardcon10@gmail.com>
2024-10-14 05:38:33 +0300

    chore: sst config for AWS SES

--- sst.config.ts
+++ sst.config.ts
@@ -1,8 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="./.sst/platform/config.d.ts" />
 
-import * as pulumi from "@pulumi/pulumi";
-
 const isProd = (stage: string) => stage.startsWith("prod");
 
 export default $config({
@@ -20,27 +18,18 @@ export default $config({
       sender: `noreply@${domainName}`,
     });
 
-    // Use pulumi.all to wait for both outputs
-    pulumi
-      .all([email.nodes.identity.arn, email.nodes.identity.configurationSetName])
-      .apply(([SES_IDENTITY_ARN, SES_CONFIGURATION_SET_NAME]) => {
-        // Now, create the Next.js app with the resolved values
-        new sst.aws.Nextjs("OpenNext", {
-          domain: {
-            name: domainName,
-          },
-          environment: {
-            AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID!,
-            AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET!,
-            AUTH_SECRET: process.env.AUTH_SECRET!,
-            AUTH_TRUST_HOST: process.env.AUTH_TRUST_HOST!,
-            DATABASE_URL: process.env.DATABASE_URL!,
-            SES_IDENTITY_ARN: SES_IDENTITY_ARN,
-            SES_CONFIGURATION_SET_NAME: SES_CONFIGURATION_SET_NAME!,
-            AWS_REGION: process.env.AWS_REGION!,
-          },
-          link: [email],
-        });
-      });
+    new sst.aws.Nextjs("OpenNext", {
+      domain: {
+        name: domainName,
+      },
+      environment: {
+        AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID!,
+        AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET!,
+        AUTH_SECRET: process.env.AUTH_SECRET!,
+        AUTH_TRUST_HOST: process.env.AUTH_TRUST_HOST!,
+        DATABASE_URL: process.env.DATABASE_URL!,
+      },
+      link: [email],
+    });
   },
 });
