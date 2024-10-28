1c8c36f05253e6fb12360076eb91f982c761cf10
bojidaryovchev <blizzardcon10@gmail.com>
2024-10-28 17:37:08 +0200

    fix: wrong import

--- src/app/verify-email/page.tsx
+++ src/app/verify-email/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import { verifyEmail } from "@/actions/auth.actions";
-import { useSearchParams } from "next/navigation";
-import { useRouter } from "next/router";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const VerifyEmailPage: React.FC = () => {
